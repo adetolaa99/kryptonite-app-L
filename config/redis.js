97d0292fe2a75905d362695a8d096e6d69bf9106
Hassan Nofisat Adetola <hassannofisat017@gmail.com>
2024-06-04 10:36:50 +0100

    updated redis config

--- config/redis.js
+++ config/redis.js
@@ -1,11 +1,11 @@
 const Redis = require("ioredis");
 const redisClient = new Redis({
   host: "127.0.0.1",
-  port: 6380,
+  port: 6379,
 });
 
 redisClient.on("connect", () => {
-  console.log("Connected to Redis");
+  console.log("Connected to Redis server!");
 });
 
 redisClient.on("error", (err) => {
